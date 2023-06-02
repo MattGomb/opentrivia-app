8385a768ce895189c27578b2c8f806b3b85c22c0
MattGomb <gombosonline@gmail.com>
2023-06-02 11:09:21 -0500

    Create enums for api call

---
+++ src/components/Types.ts
@@ -0,0 +1,40 @@
+export enum categoryList {
+  "Any Category",
+  "General Knowledge" = 9,
+  "Entertainment: books",
+  "Entertainment: film",
+  "Entertainment: music",
+  "Entertainment: musicals & theatres",
+  "Entertainment: television",
+  "Entertainment: video games",
+  "Entertainment: board games",
+  "Science & nature",
+  "Science: computers",
+  "Science: mathematics",
+  "Mythology",
+  "Sports",
+  "Geography",
+  "History",
+  "Politics",
+  "Art",
+  "Celebrities",
+  "Animals",
+  "Vehicles",
+  "Entertainment: comics",
+  "Science: gadgets",
+  "Entertainment: Japanese anime & manga",
+  "Entertainment: cartoon & animations",
+}
+
+export enum difficultyList {
+  "Any Difficulty",
+  "Easy" = "easy",
+  "Medium" = "medium",
+  "Hard" = "hard",
+}
+
+export enum typeList {
+  "Any Type",
+  "Multiple Choice" = "multiple",
+  "True / False" = "boolean",
+}
