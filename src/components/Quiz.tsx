62ee8914fc27d96b9760ba8464a085f6d381c008
MattGomb <gombosonline@gmail.com>
2023-06-03 23:45:31 -0500

    Get some random questions to display

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+"use client";
+
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 type Question = {
@@ -21,17 +23,38 @@ If category !="Any Category" api = base+&category=${category}
 If difficulty !="Any Difficulty" api = base+&difficulty=${difficulty}
 If type !="Any Type" api = base+&type=${type}
 */
-const baseApi = "https://opentdb.com/api.php?amount=${amount}";
+const baseApi = "https://opentdb.com/api.php?amount=10";
 
 const Quiz = () => {
+  const [data, setData] = useState<ApiResponse>({
+    response_code: 0,
+    results: [],
+  });
+
   const fetchQuiz = async () => {
-    const res = await axios.get<ApiResponse>(baseApi)
-    const data = res.data
+    const res = await axios.get<ApiResponse>(baseApi);
+    const data = res.data;
+    setData(data);
+
+    console.log(data.results);
+  };
+
+  useEffect(() => {
+    fetchQuiz();
+  }, []);
 
   return (
-    <div>Quiz</div>
-  )
-}
-}
+    <>
+      <h1>Quiz</h1>
+      {data.results.map((question) => (
+        <div>
+          <p key={question.question}>{question.question}</p>
+          <p>{question.correct_answer}</p>
+          <p>{question.incorrect_answers}</p>
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default Quiz
+export default Quiz;
