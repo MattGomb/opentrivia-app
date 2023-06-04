7378184d7a573d4de2ff9339a509037f35b12343
MattGomb <gombosonline@gmail.com>
2023-06-03 23:45:48 -0500

    import quiz component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Quiz from '@/components/Quiz'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Quiz />
     </main>
   )
 }
