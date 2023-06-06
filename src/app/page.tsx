75a5abb88b2044182978a39bb08c19dbab0581a4
MattGomb <gombosonline@gmail.com>
2023-06-05 20:49:32 -0500

    Add 'select' section to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Quiz from '@/components/Quiz'
+import Select from '@/components/Select'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Select />
       <Quiz />
     </main>
   )
