6d5936adcffd2aca3422ca7fc4ead5b82115c354
MattGomb <gombosonline@gmail.com>
2023-06-14 14:56:34 -0500

    Wrap layout in context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import SearchContextProvider from "@/context/ContextProvider";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'AllKindsOfTrivia app',
-  description: 'All kinds of trivia - for all kinds of people',
-}
+  title: "AllKindsOfTrivia app",
+  description: "All kinds of trivia - for all kinds of people",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SearchContextProvider>{children}</SearchContextProvider>
+      </body>
     </html>
-  )
+  );
 }
