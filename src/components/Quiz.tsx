f97279f83570fc1a06d5f3bc33418403a4a0d9f0
MattGomb <gombosonline@gmail.com>
2023-06-14 15:24:15 -0500

    Update quiz display a bit

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -5,28 +5,31 @@ import { useSearchContext } from "@/context/Context";
 const Quiz = () => {
   const { data } = useSearchContext();
 
-// We need a way to properly display quotes and stuff in the questions and answers.
   return (
     <>
       <h1 className="text-2xl">Quiz</h1>
       <div>
-        {/* If data is not yet fetched, display loading message, fix this later */
-        data.response_code != 0 ? (
-          <p>There are no 10 questions in this combination, please put together another one!</p>
-        ) : (
-        data.results.map((question) => (
-          <div className="my-4">
-            <p key={question.question} className="my-2">
-              {question.question}
+        {
+          /* If data is not yet fetched, display loading message, fix this later */
+          data.response_code != 0 ? (
+            <p>
+              There are not enough questions in this combination, please create
+              another quiz!
             </p>
-            {/* If question type is "multiple" display correct answer and incorrect array in random order
+          ) : (
+            data.results.map((question) => (
+              <div className="my-4" key={question.question}>
+                <p className="my-2">{question.question}</p>
+                {/* If question type is "multiple" display correct answer and incorrect array in random order
                 If question type is "boolean" just display true and false buttons */}
-            <p>{question.correct_answer}</p>
-            {question.incorrect_answers.map((answer) => (
-              <p>{answer}</p>
-            ))}
-          </div>
-        )))}
+                <p>{question.correct_answer}</p>
+                {question.incorrect_answers.map((answer) => (
+                  <p>{answer}</p>
+                ))}
+              </div>
+            ))
+          )
+        }
       </div>
     </>
   );
