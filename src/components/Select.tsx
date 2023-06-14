23211db587dc1e541a7a231459610ee4475d082a
MattGomb <gombosonline@gmail.com>
2023-06-14 14:55:31 -0500

    Update options, add fetch function

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -4,11 +4,16 @@ import { useSearchContext } from "@/context/Context";
 import React from "react";
 
 const Select = () => {
-  // Put here the context logic for the three variables
-  // Create onChange events for the three select elements to update the context0
-  const { category, defineCategory } = useSearchContext();
-  const { difficulty, defineDifficulty } = useSearchContext();
-  const { type, defineType } = useSearchContext();
+  const {
+    data,
+    category,
+    defineCategory,
+    difficulty,
+    defineDifficulty,
+    type,
+    defineType,
+    fetchQuiz,
+  } = useSearchContext();
 
   return (
     <>
@@ -27,35 +32,33 @@ const Select = () => {
             value={category}
             onChange={(e) => defineCategory(+e.target.value)}
           >
-            <option value="any" defaultValue={category}>
-              Any Category
-            </option>
-            <option value="9">General Knowledge</option>
-            <option value="10">Entertainment: Books</option>
-            <option value="11">Entertainment: Film</option>
-            <option value="12">Entertainment: Music</option>
-            <option value="13">Entertainment: Musicals &amp; Theatres</option>
-            <option value="14">Entertainment: Television</option>
-            <option value="15">Entertainment: Video Games</option>
-            <option value="16">Entertainment: Board Games</option>
-            <option value="17">Science &amp; Nature</option>
-            <option value="18">Science: Computers</option>
-            <option value="19">Science: Mathematics</option>
-            <option value="20">Mythology</option>
-            <option value="21">Sports</option>
-            <option value="22">Geography</option>
-            <option value="23">History</option>
-            <option value="24">Politics</option>
-            <option value="25">Art</option>
-            <option value="26">Celebrities</option>
-            <option value="27">Animals</option>
-            <option value="28">Vehicles</option>
-            <option value="29">Entertainment: Comics</option>
-            <option value="30">Science: Gadgets</option>
-            <option value="31">
+            <option value={0}>Any Category</option>
+            <option value={9}>General Knowledge</option>
+            <option value={10}>Entertainment: Books</option>
+            <option value={11}>Entertainment: Film</option>
+            <option value={12}>Entertainment: Music</option>
+            <option value={13}>Entertainment: Musicals &amp; Theatres</option>
+            <option value={14}>Entertainment: Television</option>
+            <option value={15}>Entertainment: Video Games</option>
+            <option value={16}>Entertainment: Board Games</option>
+            <option value={17}>Science &amp; Nature</option>
+            <option value={18}>Science: Computers</option>
+            <option value={19}>Science: Mathematics</option>
+            <option value={20}>Mythology</option>
+            <option value={21}>Sports</option>
+            <option value={22}>Geography</option>
+            <option value={23}>History</option>
+            <option value={24}>Politics</option>
+            <option value={25}>Art</option>
+            <option value={26}>Celebrities</option>
+            <option value={27}>Animals</option>
+            <option value={28}>Vehicles</option>
+            <option value={29}>Entertainment: Comics</option>
+            <option value={30}>Science: Gadgets</option>
+            <option value={31}>
               Entertainment: Japanese Anime &amp; Manga
             </option>
-            <option value="32">Entertainment: Cartoon &amp; Animations</option>
+            <option value={32}>Entertainment: Cartoon &amp; Animations</option>
           </select>
         </div>
         <div>
@@ -69,12 +72,10 @@ const Select = () => {
             value={difficulty}
             onChange={(e) => defineDifficulty(e.target.value)}
           >
-            <option value="any" defaultValue={difficulty}>
-              Any Difficulty
-            </option>
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
+            <option value={""}>Any Difficulty</option>
+            <option value={"easy"}>Easy</option>
+            <option value={"medium"}>Medium</option>
+            <option value={"hard"}>Hard</option>
           </select>
         </div>
         <div>
@@ -88,15 +89,19 @@ const Select = () => {
             value={type}
             onChange={(e) => defineType(e.target.value)}
           >
-            <option value="any" defaultValue={type}>
-              Any Type
-            </option>
-            <option value="multiple">Multiple Choice</option>
-            <option value="boolean">True / False</option>
+            <option value={""}>Any Type</option>
+            <option value={"multiple"}>Multiple Choice</option>
+            <option value={"boolean"}>True / False</option>
           </select>
         </div>
       </div>
-      <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+      <button
+        className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        onClick={() => {
+          fetchQuiz(data.response_code, []),
+            console.log(category, difficulty, type);
+        }}
+      >
         Get me a Quiz!
       </button>
     </>
