caf62d8c41850607d2942cb558a7216daa35cd02
MattGomb <gombosonline@gmail.com>
2023-08-09 15:41:15 -0500

    Add proper import of json

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -1,16 +1,20 @@
 import Link from "next/link";
 import Image from "next/image";
 import pubquiz from "./images/pubquiz.jpg";
-import PageTexts from "./texts/PageTexts.json";
-import QuizTexts from "./texts/QuizTexts.json";
-import TriviaTexts from "./texts/TriviaTexts.json";
-import { Texts } from "@/app/types";
 import { TiArrowBack } from "react-icons/ti";
+import { IgetQuizTexts } from "../types";
 
-const Trivia = () => {
-  const { quizTitles, quizParagraphs } = QuizTexts as Texts;
-  const { triviaTitles, triviaParagraphs } = TriviaTexts as Texts;
-  const { pageTitle, pageSubtitle } = PageTexts as Texts;
+async function getQuizTexts<IgetQuizTexts>() {
+  const res = await fetch("http://localhost:3000/api/texts", {method: "GET"})
+  if(!res.ok) {
+    throw new Error('Go fuck yourself')
+  }
+  return res.json();
+}
+
+export default async function Trivia() {
+  const Texts = await getQuizTexts<IgetQuizTexts>();
+
   return (
     <section className="prose md:prose-xl max-w-none bg-white">
       <Link href="/" className="flex justify-center p-2">
@@ -20,44 +24,46 @@ const Trivia = () => {
         />
       </Link>
       <header className="flex flex-col items-center mt-8">
-        <h1>{pageTitle}</h1>
-        <p className="italic text-base text-center px-8">
-          {pageSubtitle}
-        </p>
+        <h1>{Texts.pageTitle}</h1>
+        <p className="italic text-base text-center px-8">{Texts.pageSubtitle}</p>
       </header>
-      <div className="grid grid-cols-3">
-        <article className="flex flex-col col-span-2">
-          <h2 className="self-center">Quiz</h2>
-          <ul>
-            {quizTitles.map((quizTitles, i) => (
-              <li key={i}>
-                {quizTitles}
-                <br />
-                {quizParagraphs[i]}
-              </li>
-            ))}
-          </ul>
-          <Link
-            href="https://www.britannica.com/topic/quiz/Types-of-quizzes"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="italic font-light self-end"
-          >
-            source
-          </Link>
-        </article>
-        <Image
-          className="rounded-lg shadow-md mx-4 col-span-1 self-center"
-          src={pubquiz}
-          alt="pubquiz image"
-          placeholder="blur"
-          priority
-          width={600}
-          height={900}
-        />
+      <div className="relative">
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="md:col-span-2 p-4 md:p-0 relative z-10">
+            <article>
+              <h2 className="text-center">Quiz</h2>
+              <ul>
+                {Texts.quizTitles.map((quizTitles: string, i: number) => (
+                  <li key={i}>
+                    {quizTitles}
+                    <br />
+                    {Texts.quizParagraphs[i]}
+                  </li>
+                ))}
+              </ul>
+              <Link
+                href="https://www.britannica.com/topic/quiz/Types-of-quizzes"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="italic font-light self-end"
+              >
+                source
+              </Link>
+            </article>
+          </div>
+          <Image
+            className="rounded-lg shadow-md col-span-1 self-center object-cover hidden inset-0 md:block"
+            src={pubquiz}
+            alt="pubquiz image"
+            placeholder="blur"
+            priority
+            width={600}
+            height={900}
+          />
+        </div>
       </div>
       <div className="grid grid-cols-3">
-      <Image
+        <Image
           className="rounded-lg shadow-md mx-4 col-span-1 self-center"
           src={pubquiz}
           alt="pubquiz image"
@@ -69,11 +75,11 @@ const Trivia = () => {
         <article className="flex flex-col col-span-2">
           <h2 className="self-center">Trivia</h2>
           <ul className="ml-4">
-            {triviaTitles.map((triviaTitles, i) => (
+            {Texts.triviaTitles.map((triviaTitles: string, i: number) => (
               <li key={i}>
                 {triviaTitles}
                 <br />
-                {triviaParagraphs[i]}
+                {Texts.triviaParagraphs[i]}
               </li>
             ))}
           </ul>
@@ -90,5 +96,3 @@ const Trivia = () => {
     </section>
   );
 };
-
-export default Trivia;
